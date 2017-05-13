3fe2fc55283aa072030e7a294595179bab7eb7e9
Manuel <prezcabellomanuel@gmail.com>
2017-05-13 19:45:44 +0000

    Added Odd exercise

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(number){
+  return number % 2 === 1;
+}
+
+var threeIsOdd = isOdd(3);
+var eightIsOdd = isOdd(8);
+
+console.log("3 is odd: " + threeIsOdd);
+console.log("8 is odd: " + eightIsOdd);
+
