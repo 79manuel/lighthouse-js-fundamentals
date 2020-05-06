4216229ea902d1afeb2bb0beaf1ca9cadb5067c5
79manuel <prezcabellomanuel>
2020-05-06 14:29:54 -0700

    for ... if statements

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,22 +1,16 @@
+// If the number is a multiple of 3, print the string "Loopy" instead of the number.
+// If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
+// If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number
 
-function loopyLighthouse (range, multiples, words) {
 
-  for (var i = range[0]; i <= range[1]; i++){
-
-    if (i % multiples[0] === 0 && i % multiples[1] === 0){
-      console.log(words[0] + words[1]);
-    }
-
-    else if (i % multiples[1] === 0 ){
-      console.log(words[1]);
-    }
-
-    else if (i % multiples[0] === 0 ){
-      console.log(words[0]);
-    }
-    else{
-      console.log(i);
-    }
+for (let numbers = 100; numbers <= 200; numbers++) {
+  if (numbers % 3 === 0 && numbers % 4 === 0) {
+    console.log('Loopy Lighthouse');
+  } else if (numbers % 4 === 0) {
+    console.log('Lighthouse');
+  } else if (numbers % 3 === 0) {
+    console.log('Loopy');
+  } else {
+    console.log(numbers);
   }
 }
-console.log(loopyLighthouse([15,90], [2,5], ["Batty", "Beacon"]));
