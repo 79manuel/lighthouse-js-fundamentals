1a10e8e43c3bc92219000633e4c4543fe9d64f03
79manuel <prezcabellomanuel>
2020-05-06 14:14:00 -0700

    conditionals while and for loop

--- chorus-while.js
+++ chorus-while.js
@@ -1,10 +1,23 @@
-var chorus = "Let's dance!";
-var repeat = 0;
-while (repeat < 10) {
+const chorus = "Let\'s dance";
+let repeat = 0;
+
+while(repeat < 10){
   if (repeat === 5) {
-    console.log("*change key*");
+    console.log("Change tempo!")
   }
   console.log(chorus);
   repeat++;
 }
+
 console.log("Until the sun comes up!");
+
+console.log("-----------------");
+
+const chorus2 = 'Dance, dance baby';
+ for (let rep = 0; rep < 10; rep++){
+   if(rep === 5){
+     console.log('La la la');
+   }
+   console.log(chorus2);
+ }
+ console.log('This is the end of the song!');
