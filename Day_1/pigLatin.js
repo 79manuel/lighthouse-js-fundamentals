129d354a3719683b068323149220c16136099988
79manuel <prezcabellomanuel>
2020-05-22 10:03:00 -0700

    refactored

---
+++ Day_1/pigLatin.js
@@ -0,0 +1,13 @@
+const originalWords = process.argv.slice(2);
+let pigLatinWords = [];
+
+function pigLatinTranslator(word){
+  return word.slice(1, word.length) + word[0] + 'ay';
+}
+
+for (let i = 0; i < originalWords.length; i++){
+  pigLatinWords.push(pigLatinTranslator(originalWords[i]));
+}
+
+console.log(pigLatinWords.join(' '));
+
