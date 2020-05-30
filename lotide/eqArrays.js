9a14e93f6e829f6eaaac0f9bc1f47d88f3e5e39d
79manuel <prezcabellomanuel>
2020-05-30 12:22:54 -0700

    implementing a function that can compare two arrays

---
+++ lotide/eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${actual} !== ${expected}`)
+  }
+};
+
+function eqArrays(array1, array2){
+  let boolean = true;
+  if (array1.length !== array2.length){
+   boolean = false;
+  }
+  for (let item in array1){
+    if (array1[item] !== array2[item]){
+      boolean = false;
+    }
+  } return boolean;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
