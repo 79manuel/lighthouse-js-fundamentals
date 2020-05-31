601dfa60eb200dab569e4840049cd8cac409bea7
79manuel <prezcabellomanuel>
2020-05-30 17:51:59 -0700

    flat()

---
+++ lotide/flatten.js
@@ -0,0 +1,28 @@
+function eqArrays(array1, array2){
+  let boolean = true;
+  if (array1.length !== array2.length){
+    boolean = false;
+  }
+  for (let item in array1){
+    if (array1[item] !== array2[item]){
+      boolean = false;
+    }
+  } return boolean;
+}
+
+
+function assertArraysEqual(array1, array2, expected){
+  let arrayEq = eqArrays(array1, array2);
+  if (arrayEq === expected){
+    console.log(`✅ ✅ ✅  Assertion passed: ${arrayEq} === ${expected}`)
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${arrayEq} !== ${expected}`)
+  }
+}
+
+function flatten(array){
+  let newArray = array.flat();
+  console.log(newArray);
+}
+
+flatten([1, 2, [3, 4], 5, [6]]);
