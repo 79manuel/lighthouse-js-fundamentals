d36b3ee06c7a393728ca758e8312dfa9becff3c0
79manuel <prezcabellomanuel>
2020-06-14 15:33:33 -0700

    added mocha and chai

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,5 +1,15 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail([5,6,7]), [6, 7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+  it("returns 'Lighthouse' 'Labs' for ['Hello'', 'Lighthouse', 'Labs']", () => {
+    assert.deepStrictEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+// assertEqual(tail([5,6,7]), [6, 7]);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
