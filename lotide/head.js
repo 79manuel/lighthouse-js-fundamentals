a95b1ce378e8fc7a5297a64456b1911da44d7822
79manuel <prezcabellomanuel>
2020-06-14 13:31:50 -0700

    refactored files

--- lotide/head.js
+++ lotide/head.js
@@ -1,15 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected){
-    console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`❌ ❌ ❌  Assertion failed: ${actual} !== ${expected}`)
-  }
-};
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(1, 'Hello');
+const assertEqual = require('./assertEqual');
 
 function head(array) {
   return array[0];
