0d2fc7d35d82b4e30760f2215f929c37a76d5984
79manuel <prezcabellomanuel>
2020-06-15 12:31:57 -0700

    created file

---
+++ lotide/index.js
@@ -0,0 +1,20 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  without: without,
+  flatten: flatten,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  letterPositions: letterPositions
+};
+
